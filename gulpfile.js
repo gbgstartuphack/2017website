5fe9ca9e5f4534c23f47164f21684d0385ae7c85
Erik Axelsson <erikaxelsson1+git@gmail.com>
2020-05-01 20:07:38 +0200

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 /* Modules & variables
 -------------------------------------*/
 var gulp          = require('gulp');
-    sass          = require('gulp-sass'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    minifycss     = require('gulp-minify-css'),
-    rename        = require('gulp-rename'),
-    images        = require('gulp-imagemin');
-    uglify        = require('gulp-uglify');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var minifycss     = require('gulp-minify-css');
+var rename        = require('gulp-rename');
+var images        = require('gulp-imagemin');
+const browsersync = require("browser-sync").create();
+var uglify        = require('gulp-uglify');
 
 
 /* Paths
@@ -24,72 +25,88 @@ var paths = {
 /* Tasks
 -------------------------------------*/
 //Server
-gulp.task('express', function() {
-  var express = require('express');
-  var app = express();
-  app.use(require('connect-livereload')({port: 4002}));
-  app.use(express.static(__dirname));
-  console.log('App running frontend on port 4000\nLivereload on port 4002');
-  app.listen(4000);
-});
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "."
+    },
+    port: 4000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
 //Live-reload
 var tinylr;
-gulp.task('livereload', function() {
+const livereload = () => {
   tinylr = require('tiny-lr')();
   tinylr.listen(4002);
-});
-
+};
 
 //Styles
-gulp.task('styles', function() {
+const styles = () => {
   return gulp.src(paths.scss)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest(paths.css))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest(paths.css));
-  
-});
-
-function notifyLiveReload(event) {
+    .pipe(gulp.dest(paths.css)); 
+};
+const notifyLiveReload =  (event) => {
   var fileName = require('path').relative(__dirname, event.path);
 
-  tinylr.changed({
+  returntinylr.changed({
     body: {
       files: [fileName]
     }
   });
-}
+};
 
 // Image minifyer
-gulp.task("images", function(){
-  gulp.src(['res/images/*.jpg', 'res/images/*.png','res/images/**/*'])
-    /*.pipe(images({
-      progressive: true,
-      cache: false
-    }))*/
+
+const imagesMover = () => {
+  return gulp.src(['res/images/*.jpg', 'res/images/*.png', 'res/images/**/*'])
     .pipe(gulp.dest("build/images"));
-});
+};
 
 //JS uglify
-gulp.task('uglify', function() {
-  gulp.src(paths.js)
+const uglifyJob =  () => {
+  return gulp.src(paths.js)
     .pipe(uglify())
     .pipe(gulp.dest(paths.jsmin))
-});
+};
 
 /* Watchers
 -------------------------------------*/
-gulp.task('watch', function() {
-  gulp.watch('stylesheets/**/*.scss', ['styles']);
-  gulp.watch(paths.js, ['uglify']);
-  gulp.watch(paths.html, notifyLiveReload);
-  gulp.watch(paths.cssReload, notifyLiveReload);
-});
 
 
-gulp.task('default', ['express', 'styles', 'uglify', 'livereload', 'watch'], function() {
+// Watch files
+const watch = () => {
+  gulp.watch("stylesheets/**/*.scss", styles);
+  gulp.watch(paths.js, gulp.series(uglify));
+  gulp.watch(
+    [
+      paths.html,
+      paths.cssReload
+    ],
+    gulp.series(browserSyncReload)
+  );
+  gulp.watch("./assets/img/**/*", images);
+}
 
-});
+const defaultTasks = gulp.parallel( styles, uglifyJob, browserSync,watch);
+
+exports.imagesMover = imagesMover;
+exports.styles = styles;
+exports.uglifyJob = uglifyJob;
+exports.livereload = livereload;
+exports.watch=watch;
+
+exports.default = defaultTasks;
